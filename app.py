05ffa8a7470f9de49793a36771c3d9c1bd2a5d5d
Allie Britton <alliebritton@macbookair.dhcp.nd.edu>
2025-07-17 19:45:51 -0400

    saving my changes before mergin branches

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import joblib
 app = Flask(__name__) 
 
 # load the trained model (happens only once)
-pipeline = joblib.load("/Users/alliebritton/Desktop/csa_summer/scam_project/website/model/pipeline.pkl")
+pipeline = joblib.load("model/pipeline.pkl")
 
 # define route for the main page
 @app.route("/", methods = ["GET", "POST"])
@@ -39,25 +39,38 @@ def index():
 
     PT_EXAMPLES = {
     'a': 'Following the newest online screening introduced by The Bureau of Human Resources, you are required to download the Zoom app',
-    'p': 'Your phone Number was randomly selected from the US and Canada Numbers database and you have won $18,087.71',
+    'c': 'You agreed to the terms and conditions before using our service, so we ask you to stop all activities that violate them. Click here to unflag your account.',   
     'f': 'You will be arrested. Your data will be leaked.',
     'l': 'I am always available to help, and it\'s my pleasure to answer any questions you may have.',
-    'u': 'We are currently in urgent need of 100 employees. Our hiring team decided that you are qualified for the position.',
-    't': 'Your area code 555-XXX suggests you are eligible for this local program.',
+    'p': 'Your phone Number was randomly selected from the US and Canada Numbers database and you have won $18,087.71',
     'r': 'While we work hard to keep our network secure, we\'re asking you to help us keep your account safe.',
-    'c': 'You agreed to the terms and conditions before using our service, so we ask you to stop all activities that violate them. Click here to unflag your account.',
-    's': 'Your resume has been recommended by many online recruitment companies.'
+    's': 'Your resume has been recommended by many online recruitment companies.',
+    't': 'Your area code 555-XXX suggests you are eligible for this local program.',
+    'u': 'We are currently in urgent need of 100 employees. Our hiring team decided that you are qualified for the position.',
     }
 
     prediction = " "  # start with no prediction
     prediction_labels = " "
     predict_prob = " "
+    predict_prob_dict = " "
     if request.method == "POST":    # if user submits the form, this runs
         user_input = request.form["scam_text"]  # get the user input from the html form
         prediction = pipeline.predict([user_input])  # give the model the user input and get PTs as output
         predict_prob = pipeline.predict_proba([user_input])
-        prediction_labels = [label for label, val in zip(PT_FULL_NAMES.values(), prediction[0]) if val == 1]
-    return render_template("index.html", prediction = prediction_labels, predict_prob = predict_prob, PT_FULL_NAMES = PT_FULL_NAMES, PT_DEFINITIONS = PT_DEFINITIONS, PT_EXAMPLES = PT_EXAMPLES)   # return the predictions to the html
+        predict_prob_percent = [round(i*100, 2) for i in predict_prob[0]]
+        predict_prob_dict = {'a':predict_prob_percent[0],
+                             'p':predict_prob_percent[1],
+                             'f':predict_prob_percent[2],
+                             'l':predict_prob_percent[3],
+                             'u':predict_prob_percent[4],
+                             't':predict_prob_percent[5],
+                             'r':predict_prob_percent[6],
+                             'c':predict_prob_percent[7],
+                             's':predict_prob_percent[8],
+                             }
+        prediction_labels = [label for label, val in zip(PT_FULL_NAMES, prediction[0]) if val == 1]
+        
+    return render_template("index.html", prediction_labels = prediction_labels, predict_prob = predict_prob, PT_FULL_NAMES = PT_FULL_NAMES, PT_DEFINITIONS = PT_DEFINITIONS, PT_EXAMPLES = PT_EXAMPLES, predict_prob_dict = predict_prob_dict )   # return the predictions to the html
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port = 5001)
